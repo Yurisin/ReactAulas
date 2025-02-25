296c4b3afed9b9b0d11a51ca9bda43be3ebe1884
Yuri Botelho <yuribotelho2912@gmail.com>
2025-02-25 16:13:13 -0300

    Styled Components

--- src/componentes/IconesHeader/index.js
+++ src/componentes/IconesHeader/index.js
@@ -1,16 +1,27 @@
 import Perfil from '../../imagens/perfil.svg'
 import Sacola from '../../imagens/sacola.svg'
-import './style.css'
+import styled from 'styled-components'
+
+const Icone = styled.li`
+  margin-right: 40px;
+  width: 25px;
+  cursor: pointer;
+`
+
+const Icones = styled.ul`
+  display: flex;
+  align-items: center;
+`
 
 const icones = [Perfil, Sacola]
 
 function IconesHeader() {
   return (
-    <ul className='icones'>
-                  {icones.map ( (icone) => (
-                    <li className='icone'><img src={icone} alt='icone'></img></li>
-                  ) ) }
-                </ul>
+    <Icones>
+      {icones.map ( (icone) => (
+        <Icone><img src={icone} alt='icone'></img></Icone>
+      ) ) }
+    </Icones>
   )
 }
 
--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,13 +1,24 @@
 import logo from "../../imagens/logo.svg"
-import "./style.css"
+import styled from "styled-components"
+
+const LogoContainer = styled.div`
+  display: flex;
+  font-size: 30px;
+`
+
+const LogoImg = styled.img`
+  margin-right: 20px;
+`
+
+
 function Logo() {
   return (
-    <div className="logo">
-      <img src={logo} alt='logo' className="logo-img"></img>
+    <LogoContainer>
+      <LogoImg src={logo} alt='logo'></LogoImg>
       <p>
         <strong>Alemao</strong>Books
       </p>
-    </div>
+    </LogoContainer>
   )
 }
 
