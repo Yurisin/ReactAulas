49f5d95e111afc151b84f6a9c94f1b4ec632c2ef
Yuri Botelho <yuribotelho2912@gmail.com>
2025-02-27 14:22:18 -0300

    Implementado Ultimos Lançamentos

--- src/componentes/Pesquisa/dadosPesquisa.js
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -1,8 +1,13 @@
-import livro from '../../imagens/livro.png';
+import capa1 from '../../imagens/ConvencerAlguem90.jpg';
+import capa2 from '../../imagens/ArmasDaPersuasao.jpg';
+import capa3 from '../../imagens/ManualDoFBI.jpg';
+import capa4 from '../../imagens/TodoCorpoFala.jpg';
+
+
 
 export const livros = [
-  {nome: 'Armas em Design', id:1, src:livro},
-  {nome: 'Armas da Persuasão', id:2, src:livro},
-  {nome: 'Manual do FBI', id:3, src:livro},
-  {nome: 'O que todo corpo fala', id:4, src:livro},
+  {nome: 'Como convencer alguem em 90 segundos', id:1, src:capa1},
+  {nome: 'Armas da Persuasão', id:2, src:capa2},
+  {nome: 'Manual do FBI', id:3, src:capa3},
+  {nome: 'O que todo corpo fala', id:4, src:capa4},
 ]
---
+++ src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -0,0 +1,9 @@
+import capa1 from '../../imagens/Mindset.jpg';
+import capa2 from '../../imagens/PoderDoHabito.jpg';
+import capa3 from '../../imagens/NuncaHoraDeParar.jpg';
+
+export const livros = [
+  {nome: 'MindSet', id:1, src:capa1},
+  {nome: 'Poder do Habito', id:2, src:capa2},
+  {nome: 'Nunca é hora de parar', id:3, src:capa3},
+]
